c0e9102a020f1d2a82a13aa0c9fca056160c3a5e
mikeharrisontelia <michael.harrison@teliacompany.com>
2021-05-20 13:29:58 +0300

    Remove db

--- main.tf
+++ main.tf
@@ -4,15 +4,15 @@ terraform {
   }
 }
 
-resource "aws_db_instance" "db" {
-  allocated_storage    = 10
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  name                 = "githubdemords"
-  username             = "foo"
-  password             = "foobarbaz"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
-}
+# resource "aws_db_instance" "db" {
+#   allocated_storage    = 10
+#   engine               = "mysql"
+#   engine_version       = "5.7"
+#   instance_class       = "db.t3.micro"
+#   name                 = "githubdemords"
+#   username             = "foo"
+#   password             = "foobarbaz"
+#   parameter_group_name = "default.mysql5.7"
+#   skip_final_snapshot  = true
+# }
 
